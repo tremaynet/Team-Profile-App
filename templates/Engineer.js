febaa27bbcad2a0ae64f0c215c00f726eb05d90e
Tremayne Terry <tremaynet@gmail.com>
2020-10-25 18:51:36 -0400

    create Engineer Class by calling the Employee Class functions

---
+++ templates/Engineer.js
@@ -0,0 +1,18 @@
+const Employee = require("./Employee");
+
+
+class Engineer extends Employee {
+    constructor(name, id, email, title) {
+        super(name, id, email);
+        this.github = github;
+        this.title = title;
+    }
+    getGithub() {
+        return this.getGithub;
+    }
+    getRole() {
+        return this.title;
+    }
+}
+
+modeule.exports = Engineer;
