5ce1a6d94fbb06e8d5f6999acc5819f0fd9b160c
Tremayne Terry <tremaynet@gmail.com>
2020-10-27 18:33:18 -0400

    complete app.js

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
+//Variables
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./lib/htmlRenderer");
-
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -47,22 +42,189 @@ function runInquirerManager() {
     return inquirer
     .prompt(inputArray);
 }
+
+function runInquirerEngineer() {
+    const inputArray = [{
+        type: "input",
+        message: "What is your github?",
+        name: "github"
+    }];
+
+    return inquirer
+        .prompt(inputArray);
+}
+function runInquirerIntern() {
+    const inputArray = [{
+        type: "input",
+        message: "What school do you attend?",
+        name: "school"
+    }];
+
+    return inquirer
+        .prompt(inputArray);
+}
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+async function run() {
+    let employeeArray = [];
+    const maxTimes = 4;
+    for (i = 0; i < maxTimes; i++) {
+        const promise = new Promise((resolve, reject) => {
+            runInquirer()
+                .then(function ({ name, id, email, title }) {
 
+                    if (title === "Manager") {
+                        runInquirerManager().then(function ({ officeNumber }) {
+                            this.employee = new Manager(name, id, email, officeNumber, title);
+                            console.log(officeNumber);
+                            employeeArray.push(employee);
+                            resolve("done");
+                        });
+
+                    } else if (title === "Engineer") {
+                        runInquirerEngineer().then(function ({ github }) {
+                            this.employee = new Engineer(name, id, email, github, title);
+                            console.log(github);
+                            employeeArray.push(employee);
+                            resolve("done");
+                        });
+                    } else if (title === "Intern") {
+                        runInquirerIntern().then(function ({ school }) {
+                            this.employee = new Intern(name, id, email, school, title);
+                            console.log(school);
+                            employeeArray.push(employee);
+                            resolve("done");
+                        });
+                    }
+
+                }).catch(function (err) {
+                    console.log("There was an error.");
+                    console.log(err);
+                });
+        });
+
+        const result = await promise;
+        console.log(result);
+    }
+
+    // console.log(employeeArray.length);
+
+    function displayTitle(employee) {
+        if (employee.title === "Manager") {
+            console.log(employee.officeNumber);
+            return `office number: ${employee.officeNumber}`;
+        }
+
+        if (employee.title === "Intern") {
+            return `school: ${employee.school}`;
+        }
+
+        if (employee.title === "Engineer") {
+            return `gitHub: ${employee.github}`;
+        }
+    }
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+    function getCardHtml() {
+        let html = "";
+        for (a = 0; a < maxTimes; a++) {
+            console.log(employeeArray[a])
+            html += `<div class="card bg-dark justify-content-center align-items-center" style="width: 18rem;">
+                <div class="col card-header">
+                    <h4>${employeeArray[a].name}</h4>
+                </div>
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+                <div class="col card-header">
+                    <h4>${employeeArray[a].title}</h4 >
+                </div >
+
+                <ul class="list-group list-group-flush text">
+                    <li class="list-group-item">ID: ${employeeArray[a].id}</li>
+                    <li class="list-group-item">Email: ${employeeArray[a].email}</li>
+                    <li class="list-group-item"> ${displayTitle(employeeArray[a])}</li>
+                </ul>
+
+            </div > `;
+        }
+        return html;
+    }
+
+
+    let html = `
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+     <title>Document</title>
+
+                <style>
+                .row {
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: center;
+                margin-top: 20px;
+                margin-bottom: 20px;
+            }
+
+            .card {
+                 padding: 15px;
+                border-radius: 6px;
+                background-color: white;
+                color: lightskyblue;
+                margin: 15px;
+            }
+
+            .text {
+                padding: 15px;
+                border-radius: 6px;
+                background-color: lightskyblue;
+                color: black;
+                margin: 15px;
+            }
+
+            .col {
+                flex: 1;
+                text-align: center;
+            }
+        </style>
+    </head>
+
+     <body>
+     <nav class="navbar navbar-dark bg-dark justify-content-center align-items-center">
+     <span class="navbar-brand mb-0 h1">
+     <h1>My Team</h1>
+     </span>
+     </nav>
+     <div class="row">
+
+     ${getCardHtml()}
+
+
+     </div>
+
+     </body>
+
+    </html>
+    `;
+
+
+    console.log(html);
+    fs.writeFile('team.html', html, function (err) {
+    if (err) throw err;
+    console.log('File is created successfully.');
+    });
+}
+run()
+
+
